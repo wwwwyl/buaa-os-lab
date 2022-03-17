22d9f4769c478af7d389ba1bf68504d83779cc29
21210114 <21210114@buaa.edu.cn>
2022-03-17 15:15:33 +0800

    1

---
+++ find_warnings.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+r=1
+gcc -Wall $1 -o test 2> warning.txt
+if [ $? -eq 0 ];then r=0; fi
+grep -n warning warning.txt | sed 's/warning: //g' > result.txt
+if [ $r -eq 0 ]
+then
+	i=1
+	while [ $i -le $2 ]
+	do
+		echo $i | ./test >> result.txt
+		i=$[$i+1]	
+	done
+fi
+pwd >> result.txt
